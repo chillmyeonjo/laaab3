3f5001027c937cbc898b292f4e05d41ab94ca2e9
student <styopa0074622@gmail.com>
2023-04-11 18:46:32 +0300

    dldld

---
+++ main.py
@@ -0,0 +1,45 @@
+def a():
+    n=int(input("введите количество слов "))
+    a=''
+    for i in range(n):
+        x=str(input("введите слово "))
+        a=a+x+" "
+    print(a)
+
+a()
+
+def b():
+    a=''
+    x = str(input("введите слово "))
+    while x!='stop':
+        a = a + x + " "
+        x = str(input("введите слово "))
+    print(a)
+
+b()
+
+def c():
+    x = str(input("введите слово"))
+    if x.count("ф")>0:
+        print('вау, это редкое слово')
+    else:
+        print('упс, слово не редкое')
+
+c()
+
+
+def d():
+    from random import randint
+
+    x=0
+    while x<3:
+        a=int(randint(0,20))
+        b=int(randint(0,20))
+        print(a,"+",b,"=")
+        y=int(input("введите ответ"))
+        if a+b!=y:
+            x=x+1
+    if x==3:
+        print('игра окончена')
+
+d()
